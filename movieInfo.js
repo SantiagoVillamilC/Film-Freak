4dec1d67f002c10c4b1ef637ea48c45271bbe090
SantiagoVillamilC <santyago4993@gmail.com>
2024-04-17 00:04:35 -0500

    more specific info movie

--- movieInfo.js
+++ movieInfo.js
@@ -1,63 +1,92 @@
 // Función para obtener el ID de la película de la URL
 function obtenerIdPeliculaDeURL() {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get('id');
-  }
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get('id');
+}
 
-  // Función para hacer la solicitud a la API de TMDB y mostrar la información de la película
-  function mostrarInformacionPelicula() {
-    const movieId = obtenerIdPeliculaDeURL();
-    console.log(movieId);
-    // Aquí debes reemplazar 'tu_api_key' con tu clave de API de TMDB
-    const apiKey = 'cace972f4626db6a5ee3ae755a24b03d';
+// Función para hacer la solicitud a la API de TMDB y mostrar la información de la película
+function mostrarInformacionPelicula() {
+  const movieId = obtenerIdPeliculaDeURL();
+  console.log(movieId);
+  // Aquí debes reemplazar 'tu_api_key' con tu clave de API de TMDB
+  const apiKey = 'cace972f4626db6a5ee3ae755a24b03d';
 
-    // Hacer la solicitud a la API de TMDB para obtener la información de la película
-    fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=es-MX`)
-      .then(response => response.json())
-      .then(data => {
-        // Mostrar la información de la película en la página
-        const movieInfoDiv = document.getElementById('movieInfo');
-        movieInfoDiv.innerHTML = `
+  // Hacer la solicitud a la API de TMDB para obtener la información de la película
+  fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=es-MX`)
+    .then(response => response.json())
+    .then(data => {
+      const header = document.getElementById("header");
+      header.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${data.backdrop_path})`;
+
+      const tituloPelicula = document.getElementById('tituloPelicula');
+      tituloPelicula.textContent = `${data.title}`
+
+      const frasePelicula = document.getElementById('frasePelicula');
+      frasePelicula.textContent = `${data.tagline}`;
+
+      const posterPeli = document.getElementById('posterPeli');
+      posterPeli.innerHTML = `<img class="poster" src="https://image.tmdb.org/t/p/original/${data.poster_path}" alt="movie poster">`;
+      // Mostrar la información de la película en la página
+      const movieInfoDiv = document.getElementById('movieInfo');
+      movieInfoDiv.innerHTML = `
           <h2>${data.title}</h2>
-          <img class="poster" src="https://image.tmdb.org/t/p/w500/${data.poster_path}" alt="movie poster">
           <p><strong>Descripción:</strong> ${data.overview}</p>
-          <p><strong>Fecha de Lanzamiento:</strong> ${data.release_date}</p>
 
-          <p><strong>Presupuesto:</strong> ${data.budget}</p>
           <p><strong>Generos:</strong> ${JSON.stringify(data.genres[0].name)}</p>
-
-          <p><strong>Estado:</strong> ${data.status}</p>
-
-          <p><strong>tagline:</strong> ${data.tagline}</p>
-          <p><strong>vote_average:</strong> ${data.vote_average}</p>
-
+          
+          
+          <p id="voteAverageElement" class="rating-circle"><strong>vote_average:</strong> ${data.vote_average}</p>
+          
           <p><strong>vote_count:</strong> ${data.vote_count}</p>
+          <p><strong>revenue:</strong> ${data.revenue}</p>
+          
+          
+          
+          <p><strong>Fecha de Lanzamiento:</strong> ${data.release_date}</p>
+          <p><strong>runtime:</strong> ${data.runtime}</p>
+          <p><strong>Presupuesto:</strong> $ ${data.budget}</p>
 
           <!-- Puedes añadir más información de la película aquí -->
         `;
-        
-        let header = document.getElementById("header");
-        header.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${data.backdrop_path})`;
+      //<p><strong>Estado:</strong> ${data.status}</p>
+      //<p><strong>tagline:</strong> ${data.tagline}</p>
 
-        let tituloPelicula = document.getElementById('tituloPelicula');
-        tituloPelicula.textContent = `${data.title}`
+      let voteAverage = data.vote_average; //Puntaje de la película
+      let formattedVoteAverage = Number(voteAverage.toFixed(1));
 
-        let frasePelicula = document.getElementById('frasePelicula');
-        frasePelicula.textContent = `${data.tagline}`;
-      })
-      .catch(error => {
-        console.error('Error al obtener información de la película:', error);
-      });
-  }
+      // Función para asignar un color según el puntaje
+      function getColorForVoteAverage(formattedVoteAverage) {
+        if (formattedVoteAverage >= 7) {
+          return "green"; // Verde para puntajes altos
+        } else if (formattedVoteAverage >= 5) {
+          return "orange"; // Naranja para puntajes medios
+        } else {
+          return "red"; // Rojo para puntajes bajos
+        }
+      }
 
-  // Llamar a la función para mostrar la información de la película cuando la página se cargue
-  window.addEventListener('DOMContentLoaded', mostrarInformacionPelicula);
+      // Obtener el color correspondiente al puntaje
+      let color = getColorForVoteAverage(formattedVoteAverage);
 
-  document.addEventListener("DOMContentLoaded", function() {
-    var logoHeader = document.getElementById("logoHeader");
-    logoHeader.addEventListener("click", function() {
-        window.location.href = "index.html";
+
+      // Aplicar el color al elemento HTML que muestra el puntaje
+      let voteAverageElement = document.getElementById('voteAverageElement');
+      voteAverageElement.style.color = color;
+
+    })
+    .catch(error => {
+      console.error('Error al obtener información de la película:', error);
     });
+}
+
+// Llamar a la función para mostrar la información de la película cuando la página se cargue
+window.addEventListener('DOMContentLoaded', mostrarInformacionPelicula);
+
+document.addEventListener("DOMContentLoaded", function () {
+  var logoHeader = document.getElementById("logoHeader");
+  logoHeader.addEventListener("click", function () {
+    window.location.href = "index.html";
+  });
 });
 
 
