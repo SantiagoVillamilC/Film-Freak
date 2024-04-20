552501fdf45742f7a23e391f1b6d88f46b543fe1
SantiagoVillamilC <santyago4993@gmail.com>
2024-04-19 19:29:57 -0500

    search bar

--- search.js
+++ search.js
@@ -1,32 +1,16 @@
-const searchForm = document.getElementById('searchForm');
 const searchInput = document.getElementById('searchInput');
-const filterSelect = document.getElementById('filterSelect');
-const sortSelect = document.getElementById('sortSelect');
 const resultsContainer = document.getElementById('results');
 
-searchForm.addEventListener('submit', async function(event) {
-    event.preventDefault();
+searchInput.addEventListener('input', async function() {
     const query = searchInput.value.trim();
-    const filter = filterSelect.value;
-    const sort = sortSelect.value;
-
     const results = await searchMovies(query);
-
     displayResults(results);
 });
 
-async function searchMovies(query, filter, sort) {
+async function searchMovies(query) {
     const apiKey = 'cace972f4626db6a5ee3ae755a24b03d';
-    const apiUrl = `https://api.themoviedb.org/3/search/movie?query=${query}&language=es&api_key=${apiKey}&page=1&include_adult=false`;
-
-    // if (filter) {
-    //     apiUrl += `&with_genres=${filter}`;
-    // }
-
-    // if (sort) {
-    //     apiUrl += `&sort_by=${sort}`;
-    // }
-
+    const apiUrl = `https://api.themoviedb.org/3/search/movie?query=${query}&language=es&api_key=${apiKey}&page=1`;
+    
     const response = await fetch(apiUrl);
     const data = await response.json();
 
@@ -40,7 +24,6 @@ function displayResults(results) {
         const movieElement = document.createElement('div');
         movieElement.classList.add('movie');
         movieElement.innerHTML = `            
-
             <div class="card mb-3" style="max-width: 540px;">
                 <div class="row g-0">
                     <div class="col-md-4">
@@ -48,17 +31,14 @@ function displayResults(results) {
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
-        <h5 class="card-title">${movie.title}</h5>
-        <p class="card-text">${movie.overview}</p>
-        
-        <a href="movie.html?id=${movie.id}">Ver Detalles</a>
-      </div>
-    </div>
-  </div>
-</div>
+                            <h5 class="card-title">${movie.title}</h5>
+                            <p class="card-text">${movie.overview}</p>
+                            <a href="movie.html?id=${movie.id}">Ver Detalles</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         `;
-        
-        //<p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
         resultsContainer.appendChild(movieElement);
     });
 }
