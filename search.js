725824e6263e91bacd6fd52ab1c1c080c751da36
SantiagoVillamilC <santyago4993@gmail.com>
2024-04-24 19:03:44 -0500

    search bootstrap

--- search.js
+++ search.js
@@ -23,22 +23,25 @@ function displayResults(results) {
     results.forEach(movie => {
         const movieElement = document.createElement('div');
         movieElement.classList.add('movie');
-        movieElement.innerHTML = `            
-            <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
+        movieElement.innerHTML = `         
+               
+            <div class="card mb-3" style="max-width: 540px;" data-bs-theme="dark" >
+                <div class="row row-cols-1 row-cols-md-2 g-0">
                     <div class="col-md-4">
-                        <img src="https://image.tmdb.org/t/p/original/${movie.poster_path}" class="img-fluid rounded-start" alt="...">
+                        <img src="https://image.tmdb.org/t/p/original/${movie.poster_path}" class="img-fluid rounded-start" alt="Poster Pelicula">
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
                             <h5 class="card-title">${movie.title}</h5>
-                            <p class="card-text">${movie.overview}</p>
+                            <p class="card-text">Titulo original: ${movie.original_title}</p>
+                            <p class="card-text">Fecha de salida: ${movie.release_date}</p>
                             <a href="movie.html?id=${movie.id}">Ver Detalles</a>
                         </div>
                     </div>
                 </div>
             </div>
         `;
+        
         resultsContainer.appendChild(movieElement);
     });
 }
